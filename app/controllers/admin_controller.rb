bf806f31a8fb72eff2111b2abe1cb24a03595f5b
Романский Алексей <kirbi.zsthan@gmail.com>
2024-12-25 18:02:06 +0300

    add a table with orders to the admin panel

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,14 +1,18 @@
 class AdminController < ApplicationController
     def index
       @categories = Product.pluck(:category).uniq
-      @products = Product.all
-  
-      if params[:query].present?
-        @products = @products.where("LOWER(name) LIKE ?", "%#{params[:query].downcase}%")
-      end
-  
-      if params[:category].present?
-        @products = @products.where(category: params[:category])
+      @view_type = params[:view_type] || 'products'
+      
+      if @view_type == 'products'
+        @products = Product.all
+        if params[:query].present?
+          @products = @products.where("LOWER(name) LIKE ?", "%#{params[:query].downcase}%")
+        end
+        if params[:category].present?
+          @products = @products.where(category: params[:category])
+        end
+      else
+        @orders = Order.includes(:order_items, :user).order(created_at: :desc)
       end
   
       respond_to do |format|
