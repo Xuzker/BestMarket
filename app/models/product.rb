78ee1697cc0e53c5fe22d5adb1191bcbe5d4edc9
Романский Алексей <kirbi.zsthan@gmail.com>
2024-12-25 13:27:08 +0300

    fix minor bug

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
   validates :name, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates :image_url, presence: true, format: { with: URI::regexp(%w[http https]), message: "должен быть корректным URL" }
